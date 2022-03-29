fea31ceebfcb9b9836aaf73838daff79827eadf5
raviramos <48851099+raviramos@users.noreply.github.com>
2022-03-29 04:48:05 +0200

    added pydantic to install_requires

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email="souoravi@gmail.com",
     description="A library for handling CIF and BinaryCIF files.",
     packages=find_packages(),
-    install_requires=["numpy >= 1.11.1", "msgpack >= 1.0.3", "requests >= 2.27.1"],
+    install_requires=["numpy >= 1.11.1", "msgpack >= 1.0.3", "requests >= 2.27.1", "pydantic >= 1.9.0"],
 )
